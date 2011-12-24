74f976208648d1e722b99706f2aadc3e81d44e8a
Hamid <hamid.palo@gmail.com>
2011-12-24 15:01:09 -0500

    Updating logging to include a file logger.

--- Logging.fs
+++ Logging.fs
@@ -1,6 +1,9 @@
 module Logging
 
 open System
+open System.IO
+open System.Text
+open System.Threading
 
 /// Log levels.
 let Error = 0
@@ -28,6 +31,70 @@ let ConsoleLogger = {
     member __.Log level format =
       Printf.kprintf (printfn "[%s][%A] %s" (LevelToString level) System.DateTime.Now) format
  }
+
+/// FileLogger that writes to a specified stream or file.
+type FileLogger (stream:Stream) = class
+  /// Where we write this stuff.
+  let (file:Stream) = stream
+
+  
+  /// The encoding to use.
+  let encoding = Encoding.Unicode
+
+  
+  /// Controls whether anything is written to the file.
+  let mutable loggingEnabled = 1
+  let refLoggingEnabled = ref loggingEnabled
+
+
+  /// Write the message to the log file.
+  let writeMessageToFile (msg:string) =
+    let bytes = encoding.GetBytes(msg)
+    
+    // Don't write if logging is disabled.
+    if (Interlocked.CompareExchange(refLoggingEnabled, 0, 0) = 1) then
+      file.Write(bytes, 0, bytes.Length)
+   
+
+  /// The agent to which we post all these messages.
+  let agent =
+    new MailboxProcessor<string>(fun inbox ->
+          let rec Loop() =
+            async { let! message = inbox.Receive()
+                    writeMessageToFile message
+                    do! Loop () }
+          Loop())
+  do agent.Start()
+  
+
+  /// Posts the message to the agent.
+  let log level format =
+    // Don't post is locking is disabled.
+    if (Interlocked.CompareExchange(refLoggingEnabled, 0, 0) = 0) then ()
+    
+    // Logging is still enabled, write it out.
+    let msg =
+      sprintf "[%s][%A] %s%s"
+        (LevelToString level) System.DateTime.Now format Environment.NewLine
+    agent.Post msg
+
+
+  /// Creates an instance that logs to file.
+  new (filePath:string) =
+    let fs = File.OpenWrite(filePath)
+    FileLogger(fs)
+  
+
+  interface ILogger with
+    /// Log the message.
+    member this.Log level format = Printf.kprintf (log level) format
+
+
+  /// Stops all logging.
+  member this.StopLogging () =
+    if (Interlocked.Exchange(refLoggingEnabled, 0) = 0) then ()
+    file.Close()
+end
 
 /// Defines which logger to use.
 let mutable DefaultLogger = ConsoleLogger
